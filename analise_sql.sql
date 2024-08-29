5d162f616c950dd770e0835c577e42c6aaf95fc4
JairsonAlbertoSami <jairsonalberto@id.uff.br>
2024-08-29 10:45:51 -0300

    Responder a segunda questão

--- analise_sql.sql
+++ analise_sql.sql
@@ -7,3 +7,17 @@ WITH chamados_selecionados AS (
 SELECT COUNT(*) AS total_chamados
 FROM chamados_selecionados
 WHERE data_formatada = '2023-04-01';
+
+-- 2. Qual o tipo de chamado com mais registros no dia 01/04/2023?
+WITH chamados_selecionados AS (
+  SELECT 
+    tipo,
+    PARSE_DATE('%Y-%m-%d', FORMAT_DATE('%Y-%m-%d', CAST(data_inicio AS DATE))) AS data_formatada
+  FROM `datario.adm_central_atendimento_1746.chamado`
+)
+SELECT tipo, COUNT(*) AS total_chamados
+FROM chamados_selecionados
+WHERE data_formatada = '2023-04-01'
+GROUP BY tipo
+ORDER BY total_chamados DESC
+LIMIT 1;
