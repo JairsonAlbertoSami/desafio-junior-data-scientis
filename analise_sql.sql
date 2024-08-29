8c40f296e0221794a23884820565e7e0dba58bf0
JairsonAlbertoSami <jairsonalberto@id.uff.br>
2024-08-29 10:52:28 -0300

    Responder a terceira questão

--- analise_sql.sql
+++ analise_sql.sql
@@ -21,3 +21,20 @@ WHERE data_formatada = '2023-04-01'
 GROUP BY tipo
 ORDER BY total_chamados DESC
 LIMIT 1;
+
+
+-- 3. Nomes dos 3 bairros com mais chamados no dia 01/04/2023
+WITH chamados_selecionados AS (
+  SELECT 
+    id_bairro,
+    PARSE_DATE('%Y-%m-%d', FORMAT_DATE('%Y-%m-%d', CAST(data_inicio AS DATE))) AS data_formatada
+  FROM `datario.adm_central_atendimento_1746.chamado`
+)
+SELECT b.nome, COUNT(*) AS total_chamados
+FROM chamados_selecionados c
+JOIN `datario.dados_mestres.bairro` b
+ON c.id_bairro = b.id_bairro
+WHERE c.data_formatada = '2023-04-01'
+GROUP BY b.nome
+ORDER BY total_chamados DESC
+LIMIT 3;
